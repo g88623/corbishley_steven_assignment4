0877b1c3c2c659e1275b5c7e341f23af6994c379
g88623 <steven243@fullsail.edu>
2013-08-02 00:50:46 -0400

    Adding my first function.

--- corbishley_steven_assignment4.js
+++ corbishley_steven_assignment4.js
@@ -5,11 +5,32 @@
 
 
 
-// Library
+var myLibrary = function(){
 
 	// Strings
 
-	// Is the string a phone number (123-456-7890)
+	var checkPhone = function(val){
+		var strLen = val.length;
+		if(strLen == 12){
+
+			for (var i=0; i < strLen; i++){
+				var str = val.substring(i,i+1);
+				if(i==3 || i==7){
+					if(str != "-"){
+						return "first false";
+					}
+				} else {
+					if(str!="0" || str!="1" || str!="2" || str!="3" || str!="4" || str!="5" || str!="6" || str!="7" || str!="8" || str!="9") {
+						return "second false";
+					}
+				}
+			}
+
+		} else {
+			return false;
+		}
+	}; // Is the string a phone number (123-456-7890)
+/*
 	// Is the string an email address (abc@bbb.ccc, with only one @ and at least one . after @)
 	// Is the string a URL (https: or http:)
 	// Title-case string (split into words, then uppercase the first letter of each word)
@@ -27,7 +48,14 @@
 	// Find the smallest value in an array that is greater than a given number
 	// Find the total value of just the numbers in an array, even if some of the items are not numbers.
 	// Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
-
-	// Return Object
+*/
+	return {
+		"checkPhone": checkPhone
+	}; // Return Object
+}; // End of myLibrary
 
 // Function calls
+
+var newLibrary = new myLibrary();
+
+console.log("Is 123-456-7890 a phone number: " + newLibrary.checkPhone("123-456-7890"));
